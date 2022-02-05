cf7115e89e565bb9dec5af56e7226eddbf3c884d
Vitaly Velikodny <vvelikodny@gmail.com>
2022-02-05 15:57:36 +0300

    add options: browser_cache_settings, webp, rewrite, redirect_http_to_https, gzipOn, cors (#9)
    Co-authored-by: Vitaly Velikodny <vitaly.velikodny@gcorelabs.com>

--- gcore/options.go
+++ gcore/options.go
@@ -1,8 +1,14 @@
 package gcore
 
 type Options struct {
-	EdgeCacheSettings *EdgeCacheSettings `json:"edge_cache_settings,omitempty"`
-	HostHeader        *HostHeader        `json:"hostHeader,omitempty"`
+	EdgeCacheSettings    *EdgeCacheSettings    `json:"edge_cache_settings,omitempty"`
+	BrowserCacheSettings *BrowserCacheSettings `json:"browser_cache_settings,omitempty"`
+	HostHeader           *HostHeader           `json:"hostHeader,omitempty"`
+	Webp                 *Webp                 `json:"webp,omitempty"`
+	Rewrite              *Rewrite              `json:"rewrite,omitempty"`
+	RedirectHttpToHttps  *RedirectHttpToHttps  `json:"redirect_http_to_https,omitempty"`
+	GzipOn               *GzipOn               `json:"gzipOn,omitempty"`
+	Cors                 *Cors                 `json:"cors,omitempty"`
 }
 
 type EdgeCacheSettings struct {
@@ -15,4 +21,37 @@ type EdgeCacheSettings struct {
 type HostHeader struct {
 	Enabled bool   `json:"enabled"`
 	Value   string `json:"value"`
+}
+
+type BrowserCacheSettings struct {
+	Enabled bool   `json:"enabled"`
+	Value   string `json:"value"`
+}
+
+type Webp struct {
+	Enabled     bool `json:"enabled"`
+	JPGQuality  int  `json:"jpg_quality"`
+	PNGQuality  int  `json:"png_quality"`
+	PNGLossless bool `json:"png_lossless"`
+}
+
+type Rewrite struct {
+	Enabled bool   `json:"enabled"`
+	Body    string `json:"body"`
+	Flag    string `json:"flag"`
+}
+
+type RedirectHttpToHttps struct {
+	Enabled bool `json:"enabled"`
+	Value   bool `json:"value"`
+}
+
+type GzipOn struct {
+	Enabled bool `json:"enabled"`
+	Value   bool `json:"value"`
+}
+
+type Cors struct {
+	Enabled bool     `json:"enabled"`
+	Value   []string `json:"value"`
 }
