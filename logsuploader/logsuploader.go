333d989d9274279d118f89a24b7634f8d8c1282f
Paweł Zarębski <ppjzarebski@gmail.com>
2025-03-18 10:49:13 +0100

    CDM-1397 Add logs uploader target

--- logsuploader/logsuploader.go
+++ logsuploader/logsuploader.go
@@ -8,4 +8,9 @@ type LogsUploaderService interface {
 	PolicyGet(ctx context.Context, id int64) (*Policy, error)
 	PolicyUpdate(ctx context.Context, id int64, req *PolicyUpdateRequest) (*Policy, error)
 	PolicyDelete(ctx context.Context, policyID int64) error
+	TargetCreate(ctx context.Context, req *TargetCreateRequest) (*Target, error)
+	TargetList(ctx context.Context, limit, offset int) ([]Target, error)
+	TargetGet(ctx context.Context, id int64) (*Target, error)
+	TargetUpdate(ctx context.Context, id int64, req *TargetUpdateRequest) (*Target, error)
+	TargetDelete(ctx context.Context, targetID int64) error
 }
--- logsuploader/service.go
+++ logsuploader/service.go
@@ -76,3 +76,61 @@ func (s *Service) PolicyDelete(ctx context.Context, id int64) error {
 
 	return nil
 }
+
+func (s *Service) TargetCreate(ctx context.Context, req *TargetCreateRequest) (*Target, error) {
+	var target Target
+
+	if err := s.r.Request(ctx, http.MethodPost, "/cdn/logs_uploader/targets", req, &target); err != nil {
+		return nil, fmt.Errorf("request: %w", err)
+	}
+
+	return &target, nil
+}
+
+func (s *Service) TargetList(ctx context.Context, limit, offset int) ([]Target, error) {
+	var targets []Target
+	params := url.Values{}
+	if limit > 0 {
+		params.Add("limit", fmt.Sprintf("%d", limit))
+	}
+	if offset > 0 {
+		params.Add("offset", fmt.Sprintf("%d", offset))
+	}
+
+	path := "/cdn/logs_uploader/targets"
+	if len(params) > 0 {
+		path = fmt.Sprintf("%s?%s", path, params.Encode())
+	}
+
+	if err := s.r.Request(ctx, http.MethodGet, path, nil, &targets); err != nil {
+		return nil, fmt.Errorf("request: %w", err)
+	}
+
+	return targets, nil
+}
+
+func (s *Service) TargetGet(ctx context.Context, id int64) (*Target, error) {
+	var target Target
+	if err := s.r.Request(ctx, http.MethodGet, fmt.Sprintf("/cdn/logs_uploader/targets/%d", id), nil, &target); err != nil {
+		return nil, fmt.Errorf("request: %w", err)
+	}
+
+	return &target, nil
+}
+
+func (s *Service) TargetUpdate(ctx context.Context, id int64, req *TargetUpdateRequest) (*Target, error) {
+	var target Target
+	if err := s.r.Request(ctx, http.MethodPatch, fmt.Sprintf("/cdn/logs_uploader/targets/%d", id), req, &target); err != nil {
+		return nil, fmt.Errorf("request: %w", err)
+	}
+
+	return &target, nil
+}
+
+func (s *Service) TargetDelete(ctx context.Context, id int64) error {
+	if err := s.r.Request(ctx, http.MethodDelete, fmt.Sprintf("/cdn/logs_uploader/targets/%d", id), nil, nil); err != nil {
+		return fmt.Errorf("request: %w", err)
+	}
+
+	return nil
+}
